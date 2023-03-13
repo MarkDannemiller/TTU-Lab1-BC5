5148c06d001be08ab839ec2f3936e4e411f8b1a3
bfisch25 <brett.fischbuch@ttu.edu>
2023-03-12 21:26:03 -0500

    US Sensor

--- Nautilus_L1.srcs/sources_1/new/US_SENSOR.v
+++ Nautilus_L1.srcs/sources_1/new/US_SENSOR.v
@@ -22,7 +22,74 @@
 
 module US_SENSOR(
     input clk, //input clock for reading pulse
-    input sens_port, //port to poll on basys 
-    output [15:0] dist //output distance value
+    input echo_pin,
+    input module_enable,
+        
+    output detected,//outputs high  if something is detected in the target distance
+    output trigger//sends signal to US sensor to trigger pulse
     );
+    
+    localparam true = 1;
+    localparam false = 0;
+    
+    localparam target_distance = 10; //cm
+    reg [14:0] timer = 0; //measured in microseconds, max of 23,200
+    reg timer_enable=0;
+    reg [6:0] counter =0;
+    
+    reg[14:0] final_timer_val = 0;
+    
+    //reg initial_pulse_en = 1;
+    
+    reg[14:0] trigger_timer=0;
+    reg[6:0] trigger_counter = 0;
+    
+    reg trig_reg = 0;
+    assign trigger = trig_reg;
+    
+    assign detected = (final_timer_val<=(target_distance*58));
+    
+    //increment timer if we are receiving echo OR increment trigger timer if we are sending pulse
+    always@(posedge clk)begin
+        counter=counter+1;
+        
+        //DISABLE TIMER WHEN POSITIVE EDGE OF ECHO OCCURS
+        if(echo_pin && timer_enable) begin
+            final_timer_val = timer;
+            timer_enable = false;
+        end
+        
+        if(counter>99)begin
+            timer = timer+1;
+            counter=0;
+            end
+        
+        if(!echo_pin) begin
+            
+            trigger_counter = trigger_counter + 1;
+            
+            //MEASURE TRIGGER TIMER IN MICROSECONDS
+            if(trigger_counter > 99) begin
+                trigger_timer = trigger_timer + 1;
+            end
+            
+            if(trigger_timer > 9 && trigger_timer < 21) begin
+                trig_reg = 1;
+                timer = 0;
+                counter = 0;
+                timer_enable = true;
+            end
+            else begin
+                trig_reg = 0;
+            end
+        end
+        else begin
+            trig_reg = 0;
+            trigger_timer = 0;
+            trigger_counter = 0;
+        end
+    end
+    
+        
+    
 endmodule
